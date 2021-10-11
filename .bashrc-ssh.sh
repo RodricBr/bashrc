64e5a4a17c978bd4f0a30675771bcf2cf2a35e29
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-10-10 22:14:13 -0300

    Update .bashrc-ssh.sh

--- .bashrc-ssh.sh
+++ .bashrc-ssh.sh
@@ -6,4 +6,8 @@
 # else
 # ps1='SSH'
 
+# Modelo 1 (muito maneiro, mas a ideia não é minha)
 PS1='${debian_chroot:+($debian_chroot)}\n+ \[\033[38;5;57m\]\w\033[38;5;255m\] :: \[\033[38;5;57m\]\[\033[48;5;99m\]\u\[\033[38;5;8m\]\[\033[0m\] $ '
+
+# Modelo 2 (mais legal ainda, mas foi feito 100% por mim)
+PS1='${debian_chroot:+($debian_chroot)}\n\[\033[0;34;1m\]┌[\u⏐🎩⏐\h \[\033[0;0;41m\][SSH]\[\033[1;37m\]\[\033[00m\]\[\033[1;31m\] \w\n\[\033[00m\]\[\033[0;34;1m\]└─\[\033[00m\]\033[0;34m\]$\[\033[00m\] '
